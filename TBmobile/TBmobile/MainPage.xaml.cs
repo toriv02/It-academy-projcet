8ee30fda6af3f57b78a881136b3342ccef78658d
toriv02 <torivmalahov@gmail.com>
2024-04-20 13:26:38 +0800

    version 0.1 primitive view without logic

---
+++ TBmobile/TBmobile/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TBmobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Enter (object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TabbedPage1());
+        }
+    }
+}
